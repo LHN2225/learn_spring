923acfe449d029e13857493a97ef2b7e932df036
lhnhan <hoangnhan2506@gmail.com>
2023-01-05 15:50:21 +0700

    init spring

---
+++ ProjectLearn/src/test/java/com/NhanLearnSpring/ProjectLearn/ProjectLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NhanLearnSpring.ProjectLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
