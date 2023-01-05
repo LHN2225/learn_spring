db8a0273d8601fbdace70ff6b819beb29d3f447d
lhnhan <hoangnhan2506@gmail.com>
2023-01-05 16:28:45 +0700

    Add banner file

--- ProjectLearn/src/main/java/com/NhanLearnSpring/ProjectLearn/ProjectLearnApplication.java
+++ ProjectLearn/src/main/java/com/NhanLearnSpring/ProjectLearn/ProjectLearnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectLearnApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectLearnApplication.class, args);
 	}
 
